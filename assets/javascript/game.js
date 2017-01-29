17878470afe2cc48f371f69ac7d9ae9689fecc30
William Hansen <williamhansen@Williams-MacBook-Pro.local>
2017-01-29 15:32:14 -0800

    Complete with bare html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 // need to finish isVictory method. Maybe add pictures. get commas our of strings.q
 
 
@@ -42,7 +41,7 @@ function create_cur_hang(term, correctarr){
 		}
 	}
 	// return out oupt array
-	return outarr.toString()
+	return outarr
 }
 
 // this is the poor dude who's hanging (object)
@@ -51,15 +50,20 @@ var hangman = {
 	life: 5,
 	// this method checks if he's dead
 	isDead: function(){
-		if ( this.life < 1 )
+		if ( this.life < 1 ){
+			this.gameOver = true;
 			return true
+		}
 		else
 			return false
 	},
-	// this method needs work. will check if user won
-	isVictory; function(){
-
-	}
+	// this method will check if user won
+	isVictory: function(){
+		if (arr_find_index(create_cur_hang(hangman.term, hangman.correctarr),"_") == -1){
+			this.gameOver = true;
+			return true
+		}
+	},
 };
 
 // this is coding hangman so here are the terms
@@ -67,53 +71,73 @@ var codingterms = ["javascript", "styles", "hypertext", "markup", "language", "p
 // this selects a random term for the game
 hangman.term = codingterms[Math.floor(Math.random() * codingterms.length)];
 
+// I dont know if an instructor is reading this, but you're welcome for logging the word you're trying to guess
+console.log(hangman.term)
+
 // here im initializing arrays to be used later. Still not sure if necessary in this language
 hangman.correctarr = [];
 hangman.incorrectarr = [];
 hangman.guessarr = [];
+// generate initial hangman string report to user.
+var hangmanstr = create_cur_hang(hangman.term, hangman.correctarr).toString();
+document.getElementById("Hangman").value = hangmanstr.replace(/\,/g," ");
 
 // Start our routine for when a key is pressed
 document.onkeydown = function(event) {
-	// turn the key to lowercase for all our beautiful comparisons 
-	var keypress = String.fromCharCode(event.keyCode).toLowerCase();
-	// log the keypress just cuz
-	console.log(keypress)
-	// check if the user has already entered the key they are guessing (short term memory)
-	if (arr_find_index(hangman.guessarr, keypress) >-1) {
-		// politely inform user they have short term memory
-		document.getElementById("message").value = "You already guessed the letter " + keypress;
-	}
-	else{
-		// log the key to an array to check against future presses
-		hangman.guessarr.push(keypress);
-		// check if the keypress matches the term they are trying to guess
-		if (arr_find_index(hangman.term, keypress) >-1){
-			// log a correct keypress to use to generate our hangman string
-			hangman.correctarr.push(keypress);
-			// report to user all letters they've correctly guessed
-			document.getElementById("correct").value = hangman.correctarr.toString();
-			// generate a new hangman string to include their new correct guess
-			document.getElementById("Hangman").value = create_cur_hang(hangman.term, hangman.correctarr);
-			// message to the user that they got one right
-			document.getElementById("message").value = "Correct guess letter " + keypress;
+	// check if the game is still in session
+	if (hangman.gameOver != true){
+		// make sure the key that was pressed is a letter
+		if (event.keyCode >= 65 && event.keyCode <= 90){
+			// turn the key to lowercase for all our beautiful comparisons 
+			var keypress = String.fromCharCode(event.keyCode).toLowerCase();
+			// log the keypress
+			console.log(keypress)
+			// check if the user has already entered the key they are guessing
+			if (arr_find_index(hangman.guessarr, keypress) >-1) {
+				// politely inform user they have short term memory
+				document.getElementById("message").value = "You already guessed the letter " + keypress;
+			}
+			else{
+				// log the key to an array to check against future presses
+				hangman.guessarr.push(keypress);
+				// check if the keypress matches the term they are trying to guess
+				if (arr_find_index(hangman.term, keypress) >-1){
+					// log a correct keypress to use to generate our hangman string
+					hangman.correctarr.push(keypress);
+					// report to user all letters they've correctly guessed
+					document.getElementById("correct").value = hangman.correctarr.toString();
+					// generate a new hangman string to include their new correct guess and remove commas. report to user.
+					var hangmanstr = create_cur_hang(hangman.term, hangman.correctarr).toString();
+					document.getElementById("Hangman").value = hangmanstr.replace(/\,/g," ");
+					// message to the user that they got one right
+					document.getElementById("message").value = "Correct guess letter " + keypress;
+				}
+				else{
+					// log an incorrect keypress
+					hangman.incorrectarr.push(keypress);
+					// slightly kill the hanging man
+					hangman.life = hangman.life-1;
+					// report all letters they've incorrectly guessed
+					document.getElementById("incorrect").value = hangman.incorrectarr;
+					// message to the user that they got one wrong
+					document.getElementById("message").value = "Incorrect guess letter " + keypress;
+				}
+				// message to user the hanging mans current life level
+				document.getElementById("Remaining").value = hangman.life;
+				// check if he ded
+				if (hangman.isDead()){
+					// tell them they lost. Every other message to the user has been polite, I think a sterner message here is justified
+					document.getElementById("message").value = "GAME OVER HE'S DEAD NOW";
+				}
+				// check for a isVictory
+				if (hangman.isVictory()){
+					// tell them they won
+					document.getElementById("message").value = "You saved a life!";
+				}
+			}
 		}
 		else{
-			// log an incorrect keypress
-			hangman.incorrectarr.push(keypress);
-			// slightly kill the hanging man
-			hangman.life = hangman.life-1;
-			// report all letters they've incorrectly guessed
-			document.getElementById("incorrect").value = hangman.incorrectarr;
-			// message to the user that they got one wrong
-			document.getElementById("message").value = "Incorrect guess letter " + keypress;
-		}
-		// message to user the hanging mans current life level
-		document.getElementById("Remaining").value = hangman.life;
-
-		// check if he ded
-		if (hangman.life < 1){
-			// tell them they need to get good. Every other message to the user has been polite, I think a sterner message here is justified
-			document.getElementById("message").value = "GAME OVER HE'S DEAD NOW";
+			document.getElementById("message").value = "Invalid keypress";
 		}
 	}
 }
